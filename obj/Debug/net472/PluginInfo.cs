4b119d2bc33d99b4a2fa299484bf3af8d3a60ab8
Electrostats <Super_justy@hotmail.com>
2023-11-19 18:37:36 -0500

    correctly now

--- obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TootTallyCore
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TootTallyCore";
-        public const string PLUGIN_NAME = "TootTallyCore";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}

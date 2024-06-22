b43d88129f6f1d5a9a0251e8d9df381cd216481a
Electrostats <Super_justy@hotmail.com>
2024-06-22 00:20:40 -0400

    Added ProgressCounter class and IProgressCountable interface

---
+++ Graphics/ProgressCounter/ProgressCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TootTallyCore.Graphics.ProgressCounter
+{
+    public abstract class ProgressCounter
+    {
+        public Action<float> OnProgressCounterUpdate;
+        public Action OnProgressCounterFinish;
+        private float _progressPercent;
+        public bool IsCompleted { get; private set; }
+
+        public void Update(float progressPercent)
+        {
+            _progressPercent = progressPercent;
+            if (IsCompleted)
+            {
+                OnProgressCounterFinish?.Invoke();
+                return;
+            }
+
+            OnProgressCounterUpdate?.Invoke(_progressPercent);
+
+        }
+    }
+}

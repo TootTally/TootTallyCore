c7c19e4ccc927defad017975afc54e96a1713f6a
Electrostats <Super_justy@hotmail.com>
2024-06-22 12:51:20 -0400

    Added finished time, GetCurrentTime pull from the timer directly

--- Graphics/ProgressCounter/ProgressCounter.cs
+++ Graphics/ProgressCounter/ProgressCounter.cs
@@ -17,11 +17,12 @@ namespace TootTallyCore.Graphics.ProgressCounter
         public bool IsCompleted { get; private set; }
         public bool IsRunning { get; private set; }
         public bool IsForcedStopped { get; private set; }
-        public double GetElapsedMillisecondsTime { get; private set; }
+        public double GetCurrentElapsedTime => _timer != null ? _timer.Elapsed.TotalMilliseconds : 0;
+        public double GetFinishedElapsedTime { get; private set; }
 
         public ProgressCounter()
         {
-            GetElapsedMillisecondsTime = 0d;
+            GetFinishedElapsedTime = 0d;
             _progressPercent = 0;
             IsCompleted = IsRunning = IsForcedStopped = false;
         }
@@ -35,7 +36,6 @@ namespace TootTallyCore.Graphics.ProgressCounter
             _progressPercent = Mathf.Clamp(progressPercent, 0, 1);
 
             OnProgressCounterUpdate?.Invoke(this, _progressPercent);
-            GetElapsedMillisecondsTime = _timer.Elapsed.TotalMilliseconds;
         }
 
         public void Start()
@@ -63,7 +63,7 @@ namespace TootTallyCore.Graphics.ProgressCounter
             IsCompleted = true;
             IsRunning = false;
             _timer = null;
-            GetElapsedMillisecondsTime = _timer.Elapsed.TotalMilliseconds;
+            GetFinishedElapsedTime = _timer.Elapsed.TotalMilliseconds;
             OnProgressCounterFinish?.Invoke(this);
         }
 
